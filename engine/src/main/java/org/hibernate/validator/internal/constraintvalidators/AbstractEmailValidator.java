89aff4fc22f58f9dc878081616d6b7c5ed6bca5f
erol <erolyapici@users.noreply.github.com>
2021-03-04 16:49:36 +0300

    remove & char email validation

--- engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/AbstractEmailValidator.java
+++ engine/src/main/java/org/hibernate/validator/internal/constraintvalidators/AbstractEmailValidator.java
@@ -36,8 +36,8 @@ public class AbstractEmailValidator<A extends Annotation> implements ConstraintV
 
 	private static final int MAX_LOCAL_PART_LENGTH = 64;
 
-	private static final String LOCAL_PART_ATOM = "[a-z0-9!#$%&'*+/=?^_`{|}~\u0080-\uFFFF-]";
-	private static final String LOCAL_PART_INSIDE_QUOTES_ATOM = "([a-z0-9!#$%&'*.(),<>\\[\\]:;  @+/=?^_`{|}~\u0080-\uFFFF-]|\\\\\\\\|\\\\\\\")";
+	private static final String LOCAL_PART_ATOM = "[a-z0-9!#$%'*+/=?^_`{|}~\u0080-\uFFFF-]";
+	private static final String LOCAL_PART_INSIDE_QUOTES_ATOM = "([a-z0-9!#$%'*.(),<>\\[\\]:;  @+/=?^_`{|}~\u0080-\uFFFF-]|\\\\\\\\|\\\\\\\")";
 	/**
 	 * Regular expression for the local part of an email address (everything before '@')
 	 */
